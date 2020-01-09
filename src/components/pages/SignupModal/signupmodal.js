931917b807cdf254b203124f9af37cfa0956d7c4
Brandon Powell <brandon.joshua.powell@gmail.com>
2020-01-08 21:45:55 -0500

    first commit

--- src/components/pages/SignupModal/signupmodal.js
+++ src/components/pages/SignupModal/signupmodal.js
@@ -35,7 +35,7 @@ class App extends React.Component {
     render() {
         return (
           <div>
-            <Modal show={this.state.show} handleclose={this.hid eModal} >
+            <Modal show={this.state.show} handleclose={this.hideModal} >
                 <div className="blkOverlay">
                     {/* This is Login Form to log in to your profile */ }
                     <div className="formContent modal-main">
